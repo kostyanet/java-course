2dd8d955bdfbb03543fc1eb0dd20c6e6754602b6
Kostya Net <net.kostya@gmail.com>
2021-04-30 21:14:58 +0300

    HomeWork 1

--- src/lesson01/Lesson01Task01.java
+++ src/lesson01/Lesson01Task01.java
@@ -1,9 +1,11 @@
 package lesson01;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Lesson01Task01 {
     public static void main(String[] args) {
-        Date dateTime = new Date();
-        System.out.println("Kostya Net. Printed at " + dateTime);
+        LocalDateTime dateTime = LocalDateTime.now();
+        String dateTimeString = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        System.out.println("Kostya Net. Printed at " + dateTimeString);
     }
 }
