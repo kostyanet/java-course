6aad53f53ac7e971cdc53d9fba4a0400832be19c
Kostya Net <net.kostya@gmail.com>
2021-04-30 20:36:51 +0300

    Initial

---
+++ src/lesson01/Lesson01Task01.java
@@ -0,0 +1,9 @@
+package lesson01;
+import java.util.Date;
+
+public class Lesson01Task01 {
+    public static void main(String[] args) {
+        Date dateTime = new Date();
+        System.out.println("Kostya Net. Printed at " + dateTime);
+    }
+}
---
+++ src/lesson01/Lesson01Task02.java
@@ -0,0 +1,25 @@
+package lesson01;
+
+public class Lesson01Task02 {
+    public static void main(String[] args) {
+        runMethod1(args[0]);
+        runMethod2(args[0]);
+    }
+
+    static void runMethod1(String input) {
+        String result = "";
+        char[] chars = input.toCharArray();
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            result += chars[i];
+        }
+
+        System.out.println("First method returns: " + result);
+    }
+
+    static void runMethod2(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        String result = sb.reverse().toString();
+        System.out.println("Second method returns: " + result);
+    }
+}
