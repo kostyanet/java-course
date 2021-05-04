79a1fe6e345c2a6d95c27b6cd68e43a5ed39e31d
Kostya Net <net.kostya@gmail.com>
2021-05-04 15:15:26 +0300

    Removed redundant imports

--- src/lesson02/Lesson02Task01.java
+++ src/lesson02/Lesson02Task01.java
@@ -1,8 +1,6 @@
 package lesson02;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.DoubleStream;
 
 public class Lesson02Task01 {
     public static void main(String[] args) {
