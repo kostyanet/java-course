2812bea66c132eed29ce204b1d7d28de2d661a9f
Kostya Net <>
2021-05-21 21:51:50 +0300

    Module 1 tasks

--- src/ua/netrebenko/module1/Task1.java
+++ src/ua/netrebenko/module1/Task1.java
@@ -1,4 +1,32 @@
 package ua.netrebenko.module1;
 
+import java.util.Arrays;
+
+/**
+ * Найти количество различных элементов массива. Пример: для 1 4 5 1 1 3 ответ 4.
+ * *Создать метод, который принимает массив и возвращает количество
+ */
 public class Task1 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 3, 7, 4, 2, 9, 1, 0};
+        System.out.println("Amount of unique numbers is " + countUniqueNumbers(array));
+    }
+
+    static int countUniqueNumbers(int[] array) {
+        int pointer = 0;
+        int[] listOfUniqueNumbers = new int[array.length];
+
+        for (int item : array) {
+            if (!arrayContains(item, listOfUniqueNumbers)) {
+                listOfUniqueNumbers[pointer] = item;
+                pointer++;
+            }
+        }
+
+        return pointer - 1;
+    }
+
+    static boolean arrayContains(int item, int[] array) {
+        return Arrays.binarySearch(array, item) >= 0;
+    }
 }
--- src/ua/netrebenko/module1/Task2.java
+++ src/ua/netrebenko/module1/Task2.java
@@ -1,4 +1,38 @@
 package ua.netrebenko.module1;
 
+import java.util.Scanner;
+
+/**
+ * Ход коня по шахматной доске.
+ * Вводится текущее положение коня и клетка в которую пробуют его передвинуть за 1 ход.
+ * Программа должна проверить, возможно ли это сделать.
+ */
 public class Task2 {
+    private final static String X = "ABCDEFGH";
+    private final static String Y = "12345678";
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter current position:");
+        String current = input.nextLine();
+        System.out.println("Enter next position:");
+        String next = input.nextLine();
+
+        System.out.println("Is next position valid: " + isStepValid(current, next));
+    }
+
+    static boolean isStepValid(String currentCell, String nextCell) {
+        String current = currentCell.toUpperCase();
+        String next = nextCell.toUpperCase();
+
+        int xDiff = getDiff(current.charAt(0), next.charAt(0), Task2.X);
+        int yDiff = getDiff(current.charAt(1), next.charAt(1), Task2.Y);
+
+        return (xDiff == 2 && yDiff == 1) || (xDiff == 1 && yDiff == 2);
+    }
+
+    static int getDiff(char current, char next, String reference) {
+        return Math.abs(reference.indexOf(current) - reference.indexOf(next));
+    }
 }
