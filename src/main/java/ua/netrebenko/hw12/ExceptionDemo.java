ab57a704f2d8df1bc7870a2a94efac873e66a0c1
Kostya Net <>
2021-06-08 15:09:54 +0300

    ExceptionDemo

---
+++ src/main/java/ua/netrebenko/hw12/ExceptionDemo.java
@@ -0,0 +1,35 @@
+package ua.netrebenko.hw12;
+
+/**
+ * Examples of code which throw different types of exceptions.
+ * Since all of the given examples deal with either unchecked exceptions (according to the chart
+ * `exceptions.jpg`) or their subclasses, none of these cases assumes use of the `try-catch` statement.
+ */
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        throwClassCastException();
+        throwNullPointerException();
+        throwNumberFormatException();
+        throwArrayIndexOutOfBoundsException();
+    }
+
+    public static void throwClassCastException() {
+        Object obj = new Integer(3);
+        System.out.println((String) obj);
+    }
+
+    public static void throwNullPointerException() {
+        String x = null;
+        System.out.println(x.charAt(1));
+    }
+
+    public static void throwNumberFormatException() {
+        String x = "1.s2";
+        System.out.println(Float.parseFloat(x));
+    }
+
+    public static void throwArrayIndexOutOfBoundsException() {
+        int[] arr = {5, 7};
+        System.out.println(arr[33]);
+    }
+}
